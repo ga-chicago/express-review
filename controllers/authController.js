07d0b4e80f36f1f45232ba439969b506dbf9f48d
Reuben Ayres <ra@sent.com>
2018-10-18 17:00:40 -0500

    rudimentary login

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,7 @@ router.get('/register', async (req, res, next) => {
     message: req.session.message
   });
 })
+
 router.post('/register', async (req, res, next) => {
   try {
     const desiredUsername = req.body.username;
@@ -34,10 +35,39 @@ router.post('/register', async (req, res, next) => {
 
 })
 router.get('/login', async (req, res, next) => {
-  
+  res.render('auth/login.ejs', {
+    message: req.session.message
+  })
 })
 router.post('/login', async (req, res, next) => {
-  
+  try {
+    const foundUser = await User.findOne({
+      username: req.body.username
+    })
+    if(!foundUser) {
+      req.session.message = "Invalid username or password"; console.log("bad username");
+      res.redirect('/auth/login');
+    } else {
+      if(foundUser.password == req.body.password) {
+        req.session.loggedIn = true;
+        req.session.username = req.body.username;
+        // you could clear the  login error  message here
+        res.redirect('/sandwiches')
+      } else {
+         req.session.message = "Invalid username or password"; console.log("bad password");
+         res.redirect('/auth/login')
+      }
+    }
+  } catch(err) {
+    next(err)
+  }
 })
+
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    res.redirect('/auth/login');
+  });
+})
+
 
 module.exports = router;

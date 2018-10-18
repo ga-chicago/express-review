450ba5151dc182ce504b81faff21e6303a7d4ac5
Reuben Ayres <ra@sent.com>
2018-10-18 16:51:36 -0500

    you can register (unsecure password)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user')
 
 router.get('/register', async (req, res, next) => {
-  res.render('auth/register.ejs');
+  res.render('auth/register.ejs', {
+    message: req.session.message
+  });
 })
 router.post('/register', async (req, res, next) => {
-  res.send(req.body)
+  try {
+    const desiredUsername = req.body.username;
+    // make sure no other people have this username
+    const user = await User.find({
+      username: desiredUsername
+    })
+    console.log(user);
+    if(user.length > 0) {
+      req.session.message = "Username already taken!"
+      res.redirect('/auth/register')
+    }
+
+    else {
+      const createdUser = await User.create({
+        username: desiredUsername,
+        password: req.body.password
+      })
+      res.redirect('/sandwiches')
+    }
+
+  } catch(err) {
+    next(err)
+  }
+
 })
-router.get('/register', async (req, res, next) => {
+router.get('/login', async (req, res, next) => {
   
 })
 router.post('/login', async (req, res, next) => {
